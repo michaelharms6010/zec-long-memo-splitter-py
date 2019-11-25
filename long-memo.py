6666f0cd19d87216e2c3973ac9e24b77c7d279ba
Michael Harms <michael.harms6010@gmail.com>
2019-11-25 11:44:58 -0600

    fixed quote formatting after from addr

--- long-memo.py
+++ long-memo.py
@@ -11,7 +11,7 @@ addyTo=textList[1].rstrip("\n")
 
 for i in textList:
     hextext=i.encode("hex")
-    outstring = 'zcash-cli z_sendmany "' + addyFrom + ' "[{\\"address\\" : \\"'+addyTo+ '\\",\\"amount\\": 0.00000001, \\"memo\\":\\"'+hextext+'\\"}]"\n'
+    outstring = 'zcash-cli z_sendmany "' + addyFrom + '" "[{\\"address\\" : \\"'+addyTo+ '\\",\\"amount\\": 0.00000001, \\"memo\\":\\"'+hextext+'\\"}]"\n'
     if textList.index(i) > 1:
         output.write(outstring)
     
