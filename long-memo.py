b57a9d15e09cd607450e55ba7982e5739b40cf47
Michael Harms <michael.harms6010@gmail.com>
2019-11-25 11:44:11 -0600

    handled file import addresses

--- long-memo.py
+++ long-memo.py
@@ -1,19 +1,20 @@
 memotext = open("memotext.txt")
 
-addyFrom=input("FROM address:")
-addyTo = input("TO address:")
 output = open("output-script.txt", "w")
 
 textList = []
 for i in memotext:
     textList.append(i)
 
+addyFrom=textList[0].rstrip("\n")
+addyTo=textList[1].rstrip("\n")
 
 for i in textList:
     hextext=i.encode("hex")
     outstring = 'zcash-cli z_sendmany "' + addyFrom + ' "[{\\"address\\" : \\"'+addyTo+ '\\",\\"amount\\": 0.00000001, \\"memo\\":\\"'+hextext+'\\"}]"\n'
-    output.write(outstring)
-
+    if textList.index(i) > 1:
+        output.write(outstring)
+    
 
     # z_sendmany "$ZADDR" "[{\"amount\": 0.8, \"address\": \"$FRIEND\"}]"
 
