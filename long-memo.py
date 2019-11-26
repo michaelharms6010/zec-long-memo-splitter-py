3b5c8a3e1b3d6b6d60a6e8b80ea5121ab70a18a6
Michael Harms <michael.harms6010@gmail.com>
2019-11-26 14:12:09 -0600

    added timeout

--- long-memo.py
+++ long-memo.py
@@ -4,7 +4,7 @@ output = open("output-script.bat", "w")
 
 textList = []
 for i in memotext:
-    textList.append(i.rstrip("~"))
+    textList.append(i.replace("~", ""))
 
 addyFrom=textList[0].rstrip("\n")
 addyTo=textList[1].rstrip("\n")
@@ -14,6 +14,7 @@ for i in textList:
     outstring = 'zcash-cli z_sendmany "' + addyFrom + '" "[{\\"address\\" : \\"'+addyTo+ '\\", \\"amount\\": 0.000, \\"memo\\":\\"'+hextext+'\\"}]" 1 0.00000001\n'
     if textList.index(i) > 1:
         output.write(outstring)
+        output.write("timeout /t 150 /nobreak \n")
     
 
     # z_sendmany "$ZADDR" "[{\"amount\": 0.8, \"address\": \"$FRIEND\"}]"
